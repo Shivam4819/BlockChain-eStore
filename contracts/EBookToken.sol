46db028dbcdbbcc4f855073dc44a6f852cb1d013
Shivam4819 <shivam4819@gmail.com>
2022-01-13 20:50:11 +0530

    adds update buybook feature

--- contracts/EBookToken.sol
+++ contracts/EBookToken.sol
@@ -3,14 +3,18 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract EBookToken is ERC20{
 
-    address public admin;
+    address payable public admin;
     
     constructor(uint256 tokenAmount)ERC20('E-Book Token','EBT'){
         _mint(msg.sender, tokenAmount* (10 ** decimals()));
-        admin= msg.sender;
+        admin= payable (msg.sender);
     }
 
-    function buyToken(uint256 amount)external{
-        _transfer(admin,msg.sender,amount);
+    function buyToken()external payable{
+         require(msg.sender!=admin,"owner can not buy token");
+         require(msg.value>0,"amount can not be 0");
+        _transfer(admin,msg.sender,msg.value);
+        // _transfer(msg.sender,admin,msg.value);
+        admin.transfer(msg.value);
     }    
 }
