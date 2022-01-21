4f6ce5bfa8d209e79cb87fe8a5708a9ccb9adaa6
Shivam4819 <shivam4819@gmail.com>
2022-01-21 20:16:53 +0530

    update migration for ERC20 token

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,11 +1,10 @@
-// var SimpleStorage = artifacts.require("./SimpleStorage.sol");
-
-// module.exports = function(deployer) {
-//   deployer.deploy(SimpleStorage);
-// };
-
 var EBook = artifacts.require("./EBookShop.sol");
+var EBookToken = artifacts.require("./EBookToken.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(EBook);
+  deployer.deploy(EBookToken,10000000);
 };
+// module.exports = function(deployer) {
+//   deployer.deploy(EBookToken,10000000);
+// };
