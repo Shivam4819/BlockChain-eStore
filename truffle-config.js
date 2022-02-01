4d301fe9dbb372b44edcae2242457cf0d700d7bc
Shivam4819 <shivam4819@gmail.com>
2022-02-01 21:14:02 +0530

    adds truffle config js

--- truffle-config.js
+++ truffle-config.js
@@ -1,17 +1,34 @@
 const path = require("path");
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+const private_keys = [
+  "15ba1e6b83b464b909654805113b638db51ec56c31128a46b65f1d728743148e"
+]
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  
   compilers:{
     solc:{
       version: "^0.8.0"
     }
   },
+
   networks: {
     develop: {
       port: 8545
+    },
+
+    rinkeby: {
+      provider: () => new HDWalletProvider({
+        privateKeys: private_keys,
+        providerOrUrl: 'https://rinkeby.infura.io/v3/1e3edc31263c4463a3a69a1925b4c135',
+        numberOfAddresses: 1
+      }),
+      network_id: 4,       
+      gas: 5500000,        
+      confirmations: 2,    
+      timeoutBlocks: 200,  
+      skipDryRun: true     
     }
   }
 };
